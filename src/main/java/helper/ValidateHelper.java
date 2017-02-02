8a7f090a34307c1181ea82ecb635496d69d5435a
ohbayashi.takuya <ohbayashi.takuya@gmail.com>
2017-02-02 22:33:28 +0900

    メソッドを追加した。

--- src/main/java/helper/ValidateHelper.java
+++ src/main/java/helper/ValidateHelper.java
@@ -1,5 +1,7 @@
 package helper;
 
+import java.util.Collection;
+
 /**
  * 各種検査処理を行うヘルパークラス.
  */
@@ -18,6 +20,24 @@ public class ValidateHelper {
 	 */
 	public static boolean isNull(Object obj) {
 		return obj == null;
+	}
+
+	/**
+	 * 引数のStringオブジェクトがnullもしくは空文字か検査する.
+	 * @param s Stringオブジェクト
+	 * @return true:nullもしくは空文字 false:値がある
+	 */
+	public static boolean isEmpty(String s) {
+		return isNull(s) || s.isEmpty();
+	}
+
+	/**
+	 * 引数のコレクションがnullもしくは空か検査する.
+	 * @param c コレクション
+	 * @return true:nullもしくは空 false:値がある
+	 */
+	public static boolean isEmpty(Collection<?> c) {
+		return isNull(c) || c.isEmpty();
 	}
 
 }
--- src/test/java/helper/ValidateHelperTest.java
+++ src/test/java/helper/ValidateHelperTest.java
@@ -2,6 +2,9 @@ package helper;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 /**
@@ -18,5 +21,42 @@ public class ValidateHelperTest {
 	public void isNull_nullではない() {
 		String obj = "";
 		assertFalse(ValidateHelper.isNull(obj));
+	}
+
+	@Test
+	public void isEmpty_文字列_null() {
+		String s = null;
+		assertTrue(ValidateHelper.isEmpty(s));
+	}
+
+	@Test
+	public void isEmpty_文字列_空文字() {
+		String s = "";
+		assertTrue(ValidateHelper.isEmpty(s));
+	}
+
+	@Test
+	public void isEmpty_文字列_値あり() {
+		String s = "a";
+		assertFalse(ValidateHelper.isEmpty(s));
+	}
+
+	@Test
+	public void isEmpty_コレクション_null() {
+		List<String> c = null;
+		assertTrue(ValidateHelper.isEmpty(c));
+	}
+
+	@Test
+	public void isEmpty_コレクション_空() {
+		List<String> c = new ArrayList<>();
+		assertTrue(ValidateHelper.isEmpty(c));
+	}
+
+	@Test
+	public void isEmpty_コレクション_要素あり() {
+		List<String> c = new ArrayList<>();
+		c.add("");
+		assertFalse(ValidateHelper.isEmpty(c));
 	}
 }
