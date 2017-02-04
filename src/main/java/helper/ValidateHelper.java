cc5ad2ddbe74581e8110b097a98010e3136e3c31
ohbayashi <ohbayashi.takuya@gmail.com>
2017-02-04 11:14:15 +0900

    電話番号のパターンか検査する処理を追加した。

--- src/main/java/helper/ValidateHelper.java
+++ src/main/java/helper/ValidateHelper.java
@@ -8,62 +8,75 @@ import java.util.regex.Pattern;
  */
 public class ValidateHelper {
 
-    /**
-     * 外部からのインスタンスの生成を禁止する.
-     */
-    private ValidateHelper() {
-    }
+	/**
+	 * 外部からのインスタンスの生成を禁止する.
+	 */
+	private ValidateHelper() {
+	}
 
-    /** パターン 数字のみ */
-    public static final String PATTERN_NUMBER = "[0-9]";
+	/** パターン 数字のみ */
+	public static final String PATTERN_NUMBER = "[0-9]";
+	/** パターン 電話番号 */
+	public static final String PATTERN_TEL_NO = "[0-9-]+";
 
-    /**
-     * 引数のオブジェクトがnullか検査する.
-     *
-     * @param obj オブジェクト
-     * @return true:objがnull false:objがnullではない
-     */
-    public static boolean isNull(Object obj) {
-        return obj == null;
-    }
+	/**
+	 * 引数のオブジェクトがnullか検査する.
+	 *
+	 * @param obj オブジェクト
+	 * @return true:objがnull false:objがnullではない
+	 */
+	public static boolean isNull(Object obj) {
+		return obj == null;
+	}
 
-    /**
-     * 引数のStringオブジェクトがnullもしくは空文字か検査する.
-     *
-     * @param s Stringオブジェクト
-     * @return true:nullもしくは空文字 false:値がある
-     */
-    public static boolean isEmpty(String s) {
-        return isNull(s) || s.isEmpty();
-    }
+	/**
+	 * 引数のStringオブジェクトがnullもしくは空文字か検査する.
+	 *
+	 * @param s Stringオブジェクト
+	 * @return true:nullもしくは空文字 false:値がある
+	 */
+	public static boolean isEmpty(String s) {
+		return isNull(s) || s.isEmpty();
+	}
 
-    /**
-     * 引数のコレクションがnullもしくは空か検査する.
-     *
-     * @param c コレクション
-     * @return true:nullもしくは空 false:値がある
-     */
-    public static boolean isEmpty(Collection<?> c) {
-        return isNull(c) || c.isEmpty();
-    }
+	/**
+	 * 引数のコレクションがnullもしくは空か検査する.
+	 *
+	 * @param c コレクション
+	 * @return true:nullもしくは空 false:値がある
+	 */
+	public static boolean isEmpty(Collection<?> c) {
+		return isNull(c) || c.isEmpty();
+	}
 
-    /**
-     * 指定のパターンに一致する文字列かどうか検査する.
-     *
-     * @param value 検証対象の文字列
-     * @param pattern 正規表現文字列
-     * @return true:入力値がパターンと一致する false:入力値がパターンと一致しない
-     */
-    public static boolean isMatch(String value, String pattern) {
+	/**
+	 * 指定のパターンに一致する文字列かどうか検査する.
+	 *
+	 * @param value 検証対象の文字列
+	 * @param pattern 正規表現文字列
+	 * @return true:入力値がパターンと一致する false:入力値がパターンと一致しない
+	 */
+	public static boolean isMatch(String value, String pattern) {
 
-        if (isNull(pattern)) {
-            throw new IllegalArgumentException("パターンを指定してください。");
-        }
+		if (isNull(pattern)) {
+			throw new IllegalArgumentException("パターンを指定してください。");
+		}
 
-        if (isNull(value)) {
-            return false;
-        }
-        return Pattern.compile(pattern).matcher(value).matches();
-    }
+		if (isNull(value)) {
+			return false;
+		}
+		return Pattern.compile(pattern).matcher(value).matches();
+	}
+
+	/**
+	 * 電話番号のパターンに一致する文字列かどうか検査する.
+	 *
+	 * @param telNo 電話番号
+	 * @return true:電話番号のパターンと一致する false:電話番号ノパターンと一致しない
+	 */
+	public static boolean isTelNo(String telNo) {
+		return isMatch(telNo, PATTERN_TEL_NO);
+
+	}
 
 }
--- src/test/java/helper/ValidateHelperTest.java
+++ src/test/java/helper/ValidateHelperTest.java
@@ -12,93 +12,120 @@ import org.junit.Test;
  */
 public class ValidateHelperTest {
 
-    @Test
-    public void isNull_null() {
-        assertTrue(ValidateHelper.isNull(null));
-    }
+	@Test
+	public void isNull_null() {
+		assertTrue(ValidateHelper.isNull(null));
+	}
 
-    @Test
-    public void isNull_nullではない() {
-        String obj = "";
-        assertFalse(ValidateHelper.isNull(obj));
-    }
+	@Test
+	public void isNull_nullではない() {
+		String obj = "";
+		assertFalse(ValidateHelper.isNull(obj));
+	}
 
-    @Test
-    public void isEmpty_文字列_null() {
-        String s = null;
-        assertTrue(ValidateHelper.isEmpty(s));
-    }
+	@Test
+	public void isEmpty_文字列_null() {
+		String s = null;
+		assertTrue(ValidateHelper.isEmpty(s));
+	}
 
-    @Test
-    public void isEmpty_文字列_空文字() {
-        String s = "";
-        assertTrue(ValidateHelper.isEmpty(s));
-    }
+	@Test
+	public void isEmpty_文字列_空文字() {
+		String s = "";
+		assertTrue(ValidateHelper.isEmpty(s));
+	}
 
-    @Test
-    public void isEmpty_文字列_値あり() {
-        String s = "a";
-        assertFalse(ValidateHelper.isEmpty(s));
-    }
+	@Test
+	public void isEmpty_文字列_値あり() {
+		String s = "a";
+		assertFalse(ValidateHelper.isEmpty(s));
+	}
 
-    @Test
-    public void isEmpty_コレクション_null() {
-        List<String> c = null;
-        assertTrue(ValidateHelper.isEmpty(c));
-    }
+	@Test
+	public void isEmpty_コレクション_null() {
+		List<String> c = null;
+		assertTrue(ValidateHelper.isEmpty(c));
+	}
 
-    @Test
-    public void isEmpty_コレクション_空() {
-        List<String> c = new ArrayList<>();
-        assertTrue(ValidateHelper.isEmpty(c));
-    }
+	@Test
+	public void isEmpty_コレクション_空() {
+		List<String> c = new ArrayList<>();
+		assertTrue(ValidateHelper.isEmpty(c));
+	}
 
-    @Test
-    public void isEmpty_コレクション_要素あり() {
-        List<String> c = new ArrayList<>();
-        c.add("");
-        assertFalse(ValidateHelper.isEmpty(c));
-    }
+	@Test
+	public void isEmpty_コレクション_要素あり() {
+		List<String> c = new ArrayList<>();
+		c.add("");
+		assertFalse(ValidateHelper.isEmpty(c));
+	}
 
-    @Test
-    public void isMatch_一致する() {
-        String value = "0";
-        assertTrue(ValidateHelper.isMatch(value, ValidateHelper.PATTERN_NUMBER));
-    }
+	@Test
+	public void isMatch_一致する() {
+		String value = "0";
+		assertTrue(ValidateHelper.isMatch(value, ValidateHelper.PATTERN_NUMBER));
+	}
 
-    @Test
-    public void isMatch_一致しない() {
-        String value = "あ";
-        assertFalse(ValidateHelper.isMatch(value, ValidateHelper.PATTERN_NUMBER));
-    }
+	@Test
+	public void isMatch_一致しない() {
+		String value = "あ";
+		assertFalse(ValidateHelper.isMatch(value, ValidateHelper.PATTERN_NUMBER));
+	}
 
-    @Test
-    public void isMatch_文字列がnullである() {
-        assertFalse(ValidateHelper.isMatch(null, ValidateHelper.PATTERN_NUMBER));
-    }
+	@Test
+	public void isMatch_文字列がnullである() {
+		assertFalse(ValidateHelper.isMatch(null, ValidateHelper.PATTERN_NUMBER));
+	}
 
-    @Test
-    public void isMatch_文字列が空白である() {
-        String value = "";
-        assertFalse(ValidateHelper.isMatch(value, ValidateHelper.PATTERN_NUMBER));
-    }
+	@Test
+	public void isMatch_文字列が空白である() {
+		String value = "";
+		assertFalse(ValidateHelper.isMatch(value, ValidateHelper.PATTERN_NUMBER));
+	}
 
-    @Test(expected = IllegalArgumentException.class)
-    public void isMatch_パターンがnullである() {
-        String value = "0";
-        ValidateHelper.isMatch(value, null);
-    }
+	@Test(expected = IllegalArgumentException.class)
+	public void isMatch_パターンがnullである() {
+		String value = "0";
+		ValidateHelper.isMatch(value, null);
+	}
 
-    @Test
-    public void isMatch_パターンが空白である() {
-        String value = "0";
-        assertFalse(ValidateHelper.isMatch(value, ConvertHelper.EMPTY));
-    }
+	@Test
+	public void isMatch_パターンが空白である() {
+		String value = "0";
+		assertFalse(ValidateHelper.isMatch(value, ConvertHelper.EMPTY));
+	}
 
-    @Test
-    public void isMatch_パターンが空白である_文字列が空白である() {
-        String value = "";
-        assertTrue(ValidateHelper.isMatch(value, ConvertHelper.EMPTY));
-    }
+	@Test
+	public void isMatch_パターンが空白である_文字列が空白である() {
+		String value = "";
+		assertTrue(ValidateHelper.isMatch(value, ConvertHelper.EMPTY));
+	}
+
+	@Test
+	public void isMatchTelNo_一致する() {
+		String telNo = "012-3456-7890";
+		assertTrue(ValidateHelper.isTelNo(telNo));
+	}
+
+	@Test
+	public void isMatchTelNo_一致しない_アンダースコア() {
+		String telNo = "012_3456_7890";
+		assertFalse(ValidateHelper.isTelNo(telNo));
+	}
+
+	@Test
+	public void isMatchTelNo_一致しない_アルファベット() {
+		String telNo = "abc-defg_hijk";
+		assertFalse(ValidateHelper.isTelNo(telNo));
+	}
+
+	public void isMatchTelNo_値がnull() {
+		assertFalse(ValidateHelper.isTelNo(null));
+	}
+
+	@Test
+	public void isMatchTelNo_値が空白() {
+		ValidateHelper.isTelNo(ConvertHelper.EMPTY);
+	}
 
 }
