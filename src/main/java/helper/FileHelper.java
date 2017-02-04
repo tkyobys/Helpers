6c33528219f557e1c14e095b12f2f95614206a41
ohbayashi.takuya <ohbayashi.takuya@gmail.com>
2017-02-04 16:47:27 +0900

    ファイルを生成する処理を追加した。

--- src/main/java/helper/FileHelper.java
+++ src/main/java/helper/FileHelper.java
@@ -1,83 +1,95 @@
 package helper;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
 
 /**
  * ファイルを取り扱うヘルパークラス.
  */
 public class FileHelper {
 
-    /**
-     * 外部からのインスタンスの生成を禁止する.
-     */
-    private FileHelper() {
-    }
+	/**
+	 * 外部からのインスタンスの生成を禁止する.
+	 */
+	private FileHelper() {
+	}
 
-    /** ファイル拡張子 */
-    public enum FileExtension {
-        // CSVファイルの拡張子
-        CSV(".csv"),
-        // TSVファイルの拡張子
-        TSV(".tsv"),
-        // PDFファイルの拡張子
-        PDF(".pdf");
+	/** ファイル拡張子 */
+	public enum FileExtension {
+		// CSVファイルの拡張子
+		CSV(".csv"),
+		// TSVファイルの拡張子
+		TSV(".tsv"),
+		// PDFファイルの拡張子
+		PDF(".pdf");
 
-        private String fileExtension;
+		private String fileExtension;
 
-        FileExtension(String fileExtension) {
-            this.fileExtension = fileExtension;
-        }
+		FileExtension(String fileExtension) {
+			this.fileExtension = fileExtension;
+		}
 
-        public String getValue() {
-            return fileExtension;
-        }
-    }
+		public String getValue() {
+			return fileExtension;
+		}
+	}
 
-    /**
-     * ファイル名と拡張子を結合した文字列を生成する.
-     *
-     * @param fileName ファイル名
-     * @param fileExtension 拡張子
-     * @return ファイル名と拡張子を結合した文字列
-     */
-    public static String joinFileExtension(String fileName, FileExtension fileExtension) {
+	/**
+	 * ファイル名と拡張子を結合した文字列を生成する.
+	 *
+	 * @param fileName ファイル名
+	 * @param fileExtension 拡張子
+	 * @return ファイル名と拡張子を結合した文字列
+	 */
+	public static String joinFileExtension(String fileName, FileExtension fileExtension) {
 
-        if (ValidateHelper.isEmpty(fileName)) {
-            throw new IllegalArgumentException("ファイル名を指定してください。");
-        }
+		if(ValidateHelper.isEmpty(fileName)) {
+			throw new IllegalArgumentException("ファイル名を指定してください。");
+		}
 
-        if (ValidateHelper.isNull(fileExtension)) {
-            throw new IllegalArgumentException("ファイル拡張子を指定してください。");
-        }
+		if(ValidateHelper.isNull(fileExtension)) {
+			throw new IllegalArgumentException("ファイル拡張子を指定してください。");
+		}
 
-        return fileName + fileExtension.getValue();
-    }
+		return fileName + fileExtension.getValue();
+	}
 
-    /**
-     * 引数で渡されたファイルパス文字列に相当するファイルが存在するか検査する.
-     *
-     * @param filePath ファイルパス
-     * @return true:ファイルが存在する false:ファイルが存在しない
-     */
-    public static boolean isExists(String filePath) {
-        if (ValidateHelper.isEmpty(filePath)) {
-            throw new IllegalArgumentException("ファイルパスを指定してください。");
-        }
-        File file = new File(filePath);
-        return file.exists();
-    }
+	/**
+	 * 引数で渡されたファイルパス文字列に相当するファイルが存在するか検査する.
+	 *
+	 * @param filePath ファイルパス
+	 * @return true:ファイルが存在する false:ファイルが存在しない
+	 */
+	public static boolean isExists(String filePath) {
+		if(ValidateHelper.isEmpty(filePath)) {
+			throw new IllegalArgumentException("ファイルパスを指定してください。");
+		}
+		File file = new File(filePath);
+		return file.exists();
+	}
 
-    /**
-     * 引数で渡されたファイルパス文字列に相当するファイルを削除する.
-     *
-     * @param filePath ファイルパス
-     * @return true：ファイルの削除に成功 false：ファイルの削除に失敗
-     */
-    public static boolean remove(String filePath) {
-        if (ValidateHelper.isEmpty(filePath)) {
-            throw new IllegalArgumentException("ファイルパスを指定してください。");
-        }
-        return new File(filePath).delete();
-    }
+	public static boolean create(String filePath) throws IOException {
+		if(ValidateHelper.isEmpty(filePath)) {
+			throw new IllegalArgumentException("ファイルパスを指定してください。");
+		}
+		if(isExists(filePath)) {
+			throw new FileAlreadyExistsException(filePath + "は既に存在します。");
+		}
+		return new File(filePath).createNewFile();
+	}
+
+	/**
+	 * 引数で渡されたファイルパス文字列に相当するファイルを削除する.
+	 *
+	 * @param filePath ファイルパス
+	 * @return true：ファイルの削除に成功 false：ファイルの削除に失敗
+	 */
+	public static boolean remove(String filePath) {
+		if(ValidateHelper.isEmpty(filePath)) {
+			throw new IllegalArgumentException("ファイルパスを指定してください。");
+		}
+		return new File(filePath).delete();
+	}
 
 }
--- src/test/java/helper/FileHelperTest.java
+++ src/test/java/helper/FileHelperTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
 
 import org.junit.Test;
 
@@ -13,93 +14,130 @@ import org.junit.Test;
  */
 public class FileHelperTest {
 
-    @Test
-    public void joinFileExtension_CSV_正常() {
-        String fileName = "ファイル";
-        String actual = FileHelper.joinFileExtension(fileName, FileHelper.FileExtension.CSV);
-        assertThat(actual, is(fileName + ".csv"));
-    }
+	@Test
+	public void joinFileExtension_CSV_正常() {
+		String fileName = "ファイル";
+		String actual = FileHelper.joinFileExtension(fileName, FileHelper.FileExtension.CSV);
+		assertThat(actual, is(fileName + ".csv"));
+	}
 
-    @Test
-    public void joinFileExtension_TSV_正常() {
-        String fileName = "ファイル";
-        String actual = FileHelper.joinFileExtension(fileName, FileHelper.FileExtension.TSV);
-        assertThat(actual, is(fileName + ".tsv"));
-    }
+	@Test
+	public void joinFileExtension_TSV_正常() {
+		String fileName = "ファイル";
+		String actual = FileHelper.joinFileExtension(fileName, FileHelper.FileExtension.TSV);
+		assertThat(actual, is(fileName + ".tsv"));
+	}
 
-    @Test
-    public void joinFileExtension_PDF_正常() {
-        String fileName = "ファイル";
-        String actual = FileHelper.joinFileExtension(fileName, FileHelper.FileExtension.PDF);
-        assertThat(actual, is(fileName + ".pdf"));
-    }
+	@Test
+	public void joinFileExtension_PDF_正常() {
+		String fileName = "ファイル";
+		String actual = FileHelper.joinFileExtension(fileName, FileHelper.FileExtension.PDF);
+		assertThat(actual, is(fileName + ".pdf"));
+	}
 
-    @Test(expected = IllegalArgumentException.class)
-    public void joinFileExtension_ファイル名がnull() {
-        String fileName = null;
-        FileHelper.joinFileExtension(fileName, FileHelper.FileExtension.TSV);
-    }
+	@Test(expected = IllegalArgumentException.class)
+	public void joinFileExtension_ファイル名がnull() {
+		String fileName = null;
+		FileHelper.joinFileExtension(fileName, FileHelper.FileExtension.TSV);
+	}
 
-    @Test(expected = IllegalArgumentException.class)
-    public void joinFileExtension_ファイル名が空文字() {
-        String fileName = ConvertHelper.EMPTY;
-        FileHelper.joinFileExtension(fileName, FileHelper.FileExtension.TSV);
-    }
+	@Test(expected = IllegalArgumentException.class)
+	public void joinFileExtension_ファイル名が空文字() {
+		String fileName = ConvertHelper.EMPTY;
+		FileHelper.joinFileExtension(fileName, FileHelper.FileExtension.TSV);
+	}
 
-    @Test(expected = IllegalArgumentException.class)
-    public void joinFileExtension_ファイル拡張子がnull() {
-        String fileName = "ファイル";
-        FileHelper.joinFileExtension(fileName, null);
-    }
+	@Test(expected = IllegalArgumentException.class)
+	public void joinFileExtension_ファイル拡張子がnull() {
+		String fileName = "ファイル";
+		FileHelper.joinFileExtension(fileName, null);
+	}
 
-    @Test
-    public void isExists_文字列_ファイルが存在する() {
-        String filePath = "src/test/resources/helper/FileHelperTest/isExists_文字列.txt";
-        assertTrue(FileHelper.isExists(filePath));
-    }
+	@Test
+	public void isExists_文字列_ファイルが存在する() {
+		String filePath = "src/test/resources/helper/FileHelperTest/isExists_文字列.txt";
+		assertTrue(FileHelper.isExists(filePath));
+	}
 
-    @Test
-    public void isExists_文字列_ファイルが存在しない() {
-        String filePath = "src/test/resources/helper/FileHelperTest/not_found.txt";
-        assertFalse(FileHelper.isExists(filePath));
-    }
+	@Test
+	public void isExists_文字列_ファイルが存在しない() {
+		String filePath = "src/test/resources/helper/FileHelperTest/not_found.txt";
+		assertFalse(FileHelper.isExists(filePath));
+	}
 
-    @Test(expected = IllegalArgumentException.class)
-    public void isExists_文字列_ファイルパスがnullである() {
-        FileHelper.isExists(null);
-    }
+	@Test(expected = IllegalArgumentException.class)
+	public void isExists_文字列_ファイルパスがnullである() {
+		FileHelper.isExists(null);
+	}
 
-    @Test(expected = IllegalArgumentException.class)
-    public void isExists_文字列_ファイルパスが空文字である() {
-        FileHelper.isExists(ConvertHelper.EMPTY);
-    }
+	@Test(expected = IllegalArgumentException.class)
+	public void isExists_文字列_ファイルパスが空文字である() {
+		FileHelper.isExists(ConvertHelper.EMPTY);
+	}
 
-    @Test
-    public void remove_文字列_ファイルが存在する() {
-        String filePath = "output-test/java/helper/FileHelperTest/remove.txt";
-        try {
-            new File(filePath).createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-            fail("テストの準備に失敗しました。");
-        }
-        assertTrue(FileHelper.remove(filePath));
-    }
+	@Test
+	public void create_文字列_正常() throws IOException {
+		String filePath = "output-test/java/helper/FileHelperTest/create_文字列.txt";
+		try {
+			assertTrue(FileHelper.create(filePath));
+		} finally {
+			FileHelper.remove(filePath);
+		}
+	}
 
-    @Test
-    public void remove_文字列_ファイルが存在しない() {
-        String filePath = "output-test/java/helper/FileHelperTest/not-found.txt";
-        assertFalse(FileHelper.remove(filePath));
-    }
+	@Test(expected = IOException.class)
+	public void create_文字列_フォルダが存在しない() throws IOException {
+		String filePath = "output-test/not-found/create_文字列.txt";
+		FileHelper.create(filePath);
+	}
 
-    @Test(expected = IllegalArgumentException.class)
-    public void remove_文字列_ファイルパスがnullである() {
-        FileHelper.remove(null);
-    }
+	@Test(expected = FileAlreadyExistsException.class)
+	public void create_文字列_ファイルが存在する() throws IOException {
+		String filePath = "output-test/java/helper/FileHelperTest/create_文字列.txt";
+		try {
+			FileHelper.create(filePath);
+			assertFalse(FileHelper.create(filePath));
+		} finally {
+			FileHelper.remove(filePath);
+		}
+	}
 
-    @Test(expected = IllegalArgumentException.class)
-    public void remove_文字列_ファイルパスが空白である() {
-        FileHelper.remove(ConvertHelper.EMPTY);
-    }
+	@Test(expected = IllegalArgumentException.class)
+	public void create_文字列_ファイルパスがnullである() throws IOException {
+		FileHelper.create(null);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void create_文字列_ファイルパスが空白である() throws IOException {
+		FileHelper.create(ConvertHelper.EMPTY);
+	}
+
+	@Test
+	public void remove_文字列_ファイルが存在する() {
+		String filePath = "output-test/java/helper/FileHelperTest/remove.txt";
+		try {
+			new File(filePath).createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail("テストの準備に失敗しました。");
+		}
+		assertTrue(FileHelper.remove(filePath));
+	}
+
+	@Test
+	public void remove_文字列_ファイルが存在しない() {
+		String filePath = "output-test/java/helper/FileHelperTest/not-found.txt";
+		assertFalse(FileHelper.remove(filePath));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void remove_文字列_ファイルパスがnullである() {
+		FileHelper.remove(null);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void remove_文字列_ファイルパスが空白である() {
+		FileHelper.remove(ConvertHelper.EMPTY);
+	}
 
 }
